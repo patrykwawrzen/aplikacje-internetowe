ade046a877e92e181e1ea45067e4e702ba2040fd
Patryk Wawrzenczyk <miscooo@wp.pl>
2019-03-28 15:35:33 +0100

    Pierwsze zajecia z Java script

---
+++ calc.js
@@ -0,0 +1,13 @@
+const pow2 = X => x * x;
+const deg2rad = deg => deg * Math.PI / 180;
+
+const init =(e, a, b) =>  {
+    console.log('container:', document.getElementById('container'));
+    console.log('Loaded in', e.timeStamp, 'ms');
+    console.log(pow2(a));
+    console.log(deg2rad(b));
+}
+
+window.addEventListener('DOMContentLoaded', (ev) => init(ev, 5, 90)); {
+ 
+});
